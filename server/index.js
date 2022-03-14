df4fab640fdb2addfa33701b200ad26bf1f8ccd8
Tombh6 <tombh6@cs.colman.ac.il>
2022-03-14 11:43:37 +0200

    init server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const http=require('http')
+const fs= require('fs')
 const port=3000
 
 const server= http.createServer(function(req,res){
-
+res.write('Hello')
+res.end()
 })
 
 server.listen(port,function(error){
 if(error){
     console.log('Something went wrong', error)
+}else{
+    console.log('Sever is listening on port ' + port)
 }
 })
