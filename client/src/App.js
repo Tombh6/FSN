0b5854bce3faa56128714f333283160fb3c6d68a
cohaviyarden <yarden@moveohls.com>
2022-04-10 19:28:59 +0300

    add styled component

--- client/src/App.js
+++ client/src/App.js
@@ -16,36 +16,37 @@ function App() {
   const [loggedInUser, setLoggedInUser] = useState({});
 
   return (
-    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
-      <Router>
-        <Routes>
-          <Route path="/home">
-            <Header />
-            <Home />
-          </Route>
-          <Route exact path="/">
-            <Header />
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/allregister">
-            <Allregister />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <PrivateRoute path="/register">
-            <Register />
-          </PrivateRoute>
-          <Route path="*">
-            <h1>404 PAGE NOT FOUND!!</h1>
-          </Route>
-        </Routes>
-      </Router>
-      <NotificationContainer />
-    </UserContext.Provider>
+    <Contact/>
+    // <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+    //   <Router>
+    //     <Routes>
+    //       <Route path="/home">
+    //         <Header />
+    //         <Home />
+    //       </Route>
+    //       <Route exact path="/">
+    //         <Header />
+    //         <Home />
+    //       </Route>
+    //       <Route path="/login">
+    //         <Login />
+    //       </Route>
+    //       <Route path="/allregister">
+    //         <Allregister />
+    //       </Route>
+    //       <Route path="/contact">
+    //         <Contact />
+    //       </Route>
+    //       <PrivateRoute path="/register">
+    //         <Register />
+    //       </PrivateRoute>
+    //       <Route path="*">
+    //         <h1>404 PAGE NOT FOUND!!</h1>
+    //       </Route>
+    //     </Routes>
+    //   </Router>
+    //   <NotificationContainer />
+    // </UserContext.Provider>
   );
 }
 
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer =  () => {
+return(
+    <div>
+        <div>
+            
+        </div>
+        <div>
+            
+        </div>
+    </div>
+)
+}
+
+export default Footer;
